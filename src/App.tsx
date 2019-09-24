f63869ce8885633a16c028d13ed530c2f806646f
Maicon <maicon.marioto@gmail.com>
2019-09-24 11:35:57 -0300

    Remoção de criação de arquivos bases

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Normalize } from "styled-normalize";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import store from "./store";
+import Sessions from "./screens/Sessions";
+import { GlobalStyle } from "./components/shared/styles/GlobalStyle";
+
+const App: React.FC = () => (
+  <Provider store={store}>
+    <Normalize />
+    <GlobalStyle />
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Sessions} />
+      </Switch>
+    </Router>
+  </Provider>
+);
 
 export default App;
